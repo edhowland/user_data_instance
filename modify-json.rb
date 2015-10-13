256c8e66cf38ee0a5ec23992383725bcb6963280
edhowland <ed.howland@gmail.com>
2015-10-13 19:13:41 +0000

    \'added modify-json.rb a way to replace values of keys in JSON file with values\'

---
+++ modify-json.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+# modify-json.rb - read JSON file, replacefields with value
+
+require 'optparse'
+require 'json'
+
+def die message, usage=''
+  puts message
+  puts
+  puts usage.to_s
+  exit 1
+end
+
+options = {}
+parser = OptionParser.new do |opt|
+  opt.banner = 'Modify input JSON replacing key with new value'
+  opt.separator ''
+  opt.on('-j file', '--json file', String, 'Input JSON file') do |file|
+    options[:json] = file
+  end
+
+
+  opt.on('-o file', '--output file', String, 'Output JSON file') do |file|
+    options[:output] = file
+  end
+  opt.separator ''
+  opt.on('--verbose', 'Verbose output of before and after JSON contents') do
+    options[:verbose] = true
+  end
+  opt.on('-h', '--help', 'Display this help') do
+    puts opt
+    exit
+  end
+
+
+end
+
+parser.parse!
+
+die 'Missing input JSON file', parser unless options[:json]
+die 'Missing MIME multipart input file', parser unless options[:mime]
+die 'Missing output.json filename', parser unless options[:output]
+
+
+
+json = JSON.parse(File.read(options[:json]))
+mime = File.read(options[:mime])
+
+
+json['user_data'] = mime
+
+File.write(options[:output], json.to_json)
