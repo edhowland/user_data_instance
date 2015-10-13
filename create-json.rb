598c08214e0f894f8082ef52a9e3eb27e5bba43a
edhowland <ed.howland@gmail.com>
2015-10-13 17:09:47 +0000

    \'added all option parsing handlers to create-json.rb\'

--- create-json.rb
+++ create-json.rb
@@ -4,14 +4,30 @@
 require 'optparse'
 require 'json'
 
+options = {}
 parser = OptionParser.new do |opt|
   opt.banner = 'create-json embeds MIME multipart file in user_data field in input JSON'
+  opt.separator ''
+  opt.on('-j file', '--json file', String, 'Input JSON file') do |file|
+    options[:json] = file
+  end
+
+  opt.on('-m file', '--mime file', String, 'Input MIME multipart file') do |file|
+    options[:mime] = file
+  end
+
+  opt.on('-o file', '--output file', String, 'Output JSON file') do |file|
+    options[:output] = file
+  end
   opt.separator ''
   opt.on('-h', '--help', 'Display this help') do
     puts opt
     exit
   end
 
+
 end
 
 parser.parse!
+
+p options
