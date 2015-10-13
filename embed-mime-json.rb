69fe46bfbacf0d7ddfec3b4de6448ed9e31a8aa0
edhowland <ed.howland@gmail.com>
2015-10-13 19:45:11 +0000

    \'renamed create-json.rb to embed-mime-json.rb\'

--- embed-mime-json.rb
+++ embed-mime-json.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# create-json.rb - read JSON file, replace user_data field with contents of MIME multipart file
+# embed-mime-json.rb - read JSON file, replace user_data field with contents of MIME multipart file
 
 require 'optparse'
 require 'json'
@@ -13,7 +13,7 @@ end
 
 options = {}
 parser = OptionParser.new do |opt|
-  opt.banner = 'create-json embeds MIME multipart file in user_data field in input JSON'
+  opt.banner = 'embed-mime-json embeds MIME multipart file in user_data field in input JSON'
   opt.separator ''
   opt.on('-j file', '--json file', String, 'Input JSON file') do |file|
     options[:json] = file
